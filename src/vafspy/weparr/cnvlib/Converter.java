89246c9c966180c9852ba19a02fe848a5b85eb7f
htmlguy <htmlguy999@gmail.com>
2014-08-15 10:20:10 -0400

    Add comments, converter method, octal

--- src/vafspy/weparr/cnvlib/Converter.java
+++ src/vafspy/weparr/cnvlib/Converter.java
@@ -3,13 +3,21 @@ package vafspy.weparr.cnvlib;
 import java.util.HashMap;
 
 public class Converter {
+	// Create HashMap of objects
 	HashMap<String, Type> types = new HashMap<String, Type>();
+	// Populate map on initialization
 	public Converter() {
 		types.put("bin",new Binary());
 		types.put("hex", new Hexadecimal());
 		types.put("dec", new Decimal());
+		types.put("oct", new Octal());
 	}
+	// Conversion method.
 	public String convert(String from, String to, String value) {
-		return "";
+		int middle;
+		Type typeto = types.get(to);
+		Type typefrom = types.get(from);
+		middle = typefrom.toInt(value);
+		return typeto.fromInt(middle);
 	}
 }
