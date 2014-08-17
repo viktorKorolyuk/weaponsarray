e36a00ba6803598a237513a6e9f27a7cac570359
Viktor <viktor.korolyuk@gmail.com>
2014-08-17 13:48:01 -0400

    edit10

--- src/vafspy/weparr/PhoneNumberCoder.java
+++ src/vafspy/weparr/PhoneNumberCoder.java
@@ -1,9 +1,18 @@
 package vafspy.weparr;
 
 public class PhoneNumberCoder {
-	public String str;
 	
-
+	
+	public static void main(String[] args){
+		String str = "180-JUSTICE";
+		
+		
+		for (String retval : str.split("")){
+		//	String regx = str = str.replaceAll("[a-zA-Z]", thechar);
+	         System.out.print(retval+" ");
+	      }
+		//System.out.println(retval);
+	}
 	
 	
 }
