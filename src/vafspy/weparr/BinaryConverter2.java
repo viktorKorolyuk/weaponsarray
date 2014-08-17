15ee4173ffdd84939a7aaa43f37e47f0c73df335
htmlguy <htmlguy@mdgapton>
2014-08-17 09:33:24 -0400

    Fixed awful indentation

--- src/vafspy/weparr/BinaryConverter2.java
+++ src/vafspy/weparr/BinaryConverter2.java
@@ -1,4 +1,5 @@
 package vafspy.weparr;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
@@ -19,36 +20,39 @@ import javax.swing.JTextPane;
 import javax.swing.UIManager;
 import javax.swing.text.StyledDocument;
 
-
 public class BinaryConverter2 extends JFrame {
 	// Add a serialVersionUID: Takes care of those pesky warnings!
 	private static final long serialVersionUID = 1L;
 	public JFrame frame;
 	public JComboBox<String> comboBox;
-	
+
 	public int frameSx = 200;
 	public int frameSy = 200;
-	
+
 	public JLabel titleText;
 	public JLabel title;
-	
+
 	public JPanel main;
 	public JTextPane data;
 	public JTextField input;
 	public JTextField output;
-	//public JScrollPane scrollpane;
+	// public JScrollPane scrollpane;
 	public StyledDocument document;
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		new BinaryConverter2();
+	}
+
+	public BinaryConverter2() {
+		frame = new JFrame();
+		frame.setTitle("Binary Converter v2");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception ex) {
 		}
-		public BinaryConverter2()
-		{	frame = new JFrame();
-			frame.setTitle("Binary Converter v2");
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.getContentPane().setLayout(null);
-			try{UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());}catch(Exception ex){}
-		
+
 		input = new JTextField();
 		input.setEditable(true);
 		input.setCaretColor(Color.BLUE);
@@ -57,37 +61,43 @@ public class BinaryConverter2 extends JFrame {
 		input.setCaretColor(Color.RED);
 		input.setOpaque(true);
 		input.setFont(new Font("Courier New", Font.BOLD, 12));
-		input.addActionListener(new ActionListener()
-		{
+		input.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e)
-			{
+			public void actionPerformed(ActionEvent e) {
 				String binary1 = input.getText();
-				try{
-					  doCommand(binary1);
-					
-				    int i = Integer.parseInt(binary1);               //hex to decimal
-				    String binary = Integer.toBinaryString(i);       //decimal to binary
-				    /**
-				    System.out.println("This is Binary: " + binary);
-				    */
-				    try {input.setText("");}
-				    catch (Exception ex) {}
-				  
-				    data.setText("");
-				    data.setText(binary);
-				input.selectAll();
-				output.setText("");
-				 doCommand(binary1);
+				try {
+					doCommand(binary1);
+
+					int i = Integer.parseInt(binary1); // hex to decimal
+					String binary = Integer.toBinaryString(i); // decimal to
+																// binary
+					/**
+					 * System.out.println("This is Binary: " + binary);
+					 */
+					try {
+						input.setText("");
+					} catch (Exception ex) {
+					}
+
+					data.setText("");
+					data.setText(binary);
+					input.selectAll();
+					output.setText("");
+					doCommand(binary1);
 				}
-				
-				catch(Exception ex) {
-					 JOptionPane.showMessageDialog(null,"Error: -->: " + ex.getMessage() + " |Please put digits as numbers do not work", "Alert", JOptionPane.ERROR_MESSAGE);
-				 }
-			}	
+
+				catch (Exception ex) {
+					JOptionPane.showMessageDialog(
+							null,
+							"Error: -->: "
+									+ ex.getMessage()
+									+ " |Please put digits as numbers do not work",
+							"Alert", JOptionPane.ERROR_MESSAGE);
+				}
+			}
 		});
-		//input.getText();
-		
+		// input.getText();
+
 		output = new JTextField();
 		output.setEditable(true);
 		output.setBounds(10, 130, 150, 25);
@@ -95,75 +105,82 @@ public class BinaryConverter2 extends JFrame {
 		output.setCaretColor(Color.RED);
 		output.setOpaque(true);
 		output.setFont(new Font("Courier New", Font.BOLD, 12));
-		//input.getText();
-		output.addActionListener(new ActionListener()
-		{
+		// input.getText();
+		output.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e)
-			{	String binary2 = output.getText();
-				 try {
+			public void actionPerformed(ActionEvent e) {
+				String binary2 = output.getText();
+				try {
 					doCommand(binary2);
-					
-				 int i = Integer.parseInt(binary2);               //hex to decimal
-				    String binary3 = Integer.toHexString(i);       //decimal to binary
-				    /**
-				    System.out.println("This is Binary: " + binary3);
-				    */
-				    try {output.setText("");}
-				    catch (Exception ex) {}
-				    data.setText("");
-				    data.setText(binary3);
-				    output.selectAll();	
-				    input.setText("");
-				 }
-				 catch(Exception ex) {
-					 JOptionPane.showMessageDialog(null,"Error: " + ex.getMessage() + " Please put digits as numbers do not work", "Alert", JOptionPane.ERROR_MESSAGE);
-				 }}});
 
-		
+					int i = Integer.parseInt(binary2); // hex to decimal
+					String binary3 = Integer.toHexString(i); // decimal to
+																// binary
+					/**
+					 * System.out.println("This is Binary: " + binary3);
+					 */
+					try {
+						output.setText("");
+					} catch (Exception ex) {
+					}
+					data.setText("");
+					data.setText(binary3);
+					output.selectAll();
+					input.setText("");
+				} catch (Exception ex) {
+					JOptionPane.showMessageDialog(
+							null,
+							"Error: "
+									+ ex.getMessage()
+									+ " Please put digits as numbers do not work",
+							"Alert", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+
 		title = new JLabel();
 		title.setText("10| Binary And Hex |01");
 		title.setFont(new Font("Courier New", Font.BOLD, 15));
 		title.setForeground(Color.BLACK);
 		title.setBounds(5, 5, 500, 20);
-		title.addMouseListener(new MouseListener(){
+		title.addMouseListener(new MouseListener() {
 
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				// TODO Auto-generated method stub
 				data.setText("Watcha looking at ya big baby?");
-				
+
 			}
 
 			@Override
 			public void mousePressed(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void mouseExited(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 		});
-		
+
 		titleText = new JLabel();
 		titleText.setText("");
-		
+
 		JButton btnBack = new JButton("Back");
 		btnBack.setBounds(540, 250, 100, 50);
 		btnBack.addActionListener(new ActionListener() {
@@ -171,127 +188,154 @@ public class BinaryConverter2 extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				new mainPass();
 				frame.dispose();
-				}
-			});
-		
+			}
+		});
+
 		frame.add(btnBack);
-		
+
 		JButton btnQuit = new JButton("Quit");
 		btnQuit.setBounds(540, 310, 100, 50);
 		btnQuit.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-			//	new mainPass();
+				// new mainPass();
 				frame.dispose();
-				}
-			});
-		
+			}
+		});
+
 		frame.add(btnQuit);
-		
+
 		JButton btnGen = new JButton("1 - 2");
 		btnGen.setBounds(10, 168, 90, 25);
-		btnGen.addActionListener(new ActionListener()
-		{
+		btnGen.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				String binary1 = input.getText();
-				try{
+				try {
 					doCommand(binary1);
-				    int i = Integer.parseInt(binary1);               //hex to decimal
-				    String binary = Integer.toBinaryString(i);       //decimal to binary
-				    /**
-				    System.out.println("This is Binary: " + binary);
-				    */
-				    try {input.setText("");}
-				    catch (Exception ex) {}
-				    data.setText("");
-				    data.setText(binary);
-				input.selectAll();
-				output.setText("");
+					int i = Integer.parseInt(binary1); // hex to decimal
+					String binary = Integer.toBinaryString(i); // decimal to
+																// binary
+					/**
+					 * System.out.println("This is Binary: " + binary);
+					 */
+					try {
+						input.setText("");
+					} catch (Exception ex) {
+					}
+					data.setText("");
+					data.setText(binary);
+					input.selectAll();
+					output.setText("");
 				}
-				
-				catch(Exception ex) {
-					 JOptionPane.showMessageDialog(null,"Error: " + ex.getMessage() + "Please put digits as numbers do not work", "Alert", JOptionPane.ERROR_MESSAGE);
-				 }
-			}	
+
+				catch (Exception ex) {
+					JOptionPane.showMessageDialog(
+							null,
+							"Error: "
+									+ ex.getMessage()
+									+ "Please put digits as numbers do not work",
+							"Alert", JOptionPane.ERROR_MESSAGE);
+				}
+			}
 		});
-		
-	   frame.getContentPane().add(btnGen);
-			
-	   JButton btnGen2 = new JButton("2 - 1");
+
+		frame.getContentPane().add(btnGen);
+
+		JButton btnGen2 = new JButton("2 - 1");
 		btnGen2.setBounds(100, 168, 90, 25);
-		btnGen2.addActionListener(new ActionListener()
-		{
+		btnGen2.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				String binary2 = output.getText();
-				 try {
-					 doCommand(binary2);
-				 int i = Integer.parseInt(binary2);               //hex to decimal
-				    String binary3 = Integer.toHexString(i);       //decimal to binary
-				    /**
-				    System.out.println("This is Binary: " + binary3);
-				    */
-				    try {output.setText("");}
-				    catch (Exception ex) {}
-				    data.setText("");
-				    data.setText(binary3);
-				    output.selectAll();
-				    input.setText("");
-				 }
-				 catch(Exception ex) {
-					 JOptionPane.showMessageDialog(null,"Error: " + ex.getMessage() + " Please put digits as numbers do not work", "Alert", JOptionPane.ERROR_MESSAGE);
-				 }
-				 output.selectAll();	
+				try {
+					doCommand(binary2);
+					int i = Integer.parseInt(binary2); // hex to decimal
+					String binary3 = Integer.toHexString(i); // decimal to
+																// binary
+					/**
+					 * System.out.println("This is Binary: " + binary3);
+					 */
+					try {
+						output.setText("");
+					} catch (Exception ex) {
+					}
+					data.setText("");
+					data.setText(binary3);
+					output.selectAll();
+					input.setText("");
+				} catch (Exception ex) {
+					JOptionPane.showMessageDialog(
+							null,
+							"Error: "
+									+ ex.getMessage()
+									+ " Please put digits as numbers do not work",
+							"Alert", JOptionPane.ERROR_MESSAGE);
+				}
+				output.selectAll();
 			}
 		});
-		
-		
-		//Choices
+
+		// Choices
 		comboBox = new JComboBox<String>();
-		comboBox.setModel(new DefaultComboBoxModel<String>(new String[] {"Bin", "Hex"}));
+		comboBox.setModel(new DefaultComboBoxModel<String>(new String[] {
+				"Bin", "Hex" }));
 		comboBox.setBounds(100, 50, 70, 24);
-		//comboBox.set
+		// comboBox.set
 		frame.getContentPane().add(comboBox);
-		
-		
-	   frame.getContentPane().add(btnGen2);
-			
+
+		frame.getContentPane().add(btnGen2);
+
 		frame.add(input, BorderLayout.SOUTH);
 		frame.add(output, BorderLayout.SOUTH);
 		frame.add(data, BorderLayout.SOUTH);
 		frame.getContentPane().add(title);
-		
-		//frame.getContentPane().setBackground(new Color(201,201,201));
-		frame.setSize(frameSx,frameSy);
+
+		// frame.getContentPane().setBackground(new Color(201,201,201));
+		frame.setSize(frameSx, frameSy);
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
 		frame.setVisible(true);
 	}
-	public void doCommand(String binary1)
-	{
+
+	public void doCommand(String binary1) {
 		final String[] commands = binary1.split(" ");
-		 try
-			{
-				if (commands[0].equalsIgnoreCase("close"))
-				{
-					data.setText("Hacker");
-					JOptionPane.showMessageDialog(null, "Accses Granted", "Alert", JOptionPane.INFORMATION_MESSAGE);
-					JOptionPane.showMessageDialog(null, "Nope", "Alert", JOptionPane.INFORMATION_MESSAGE);
-					JOptionPane.showMessageDialog(null, "Its just a binary converter - geez - Stop being a big baby", "Alert", JOptionPane.INFORMATION_MESSAGE);
-					//JOptionPane.showMessageDialog(null, "Installing Virus...101010101", "Alert", JOptionPane.INFORMATION_MESSAGE);
-					
-					frame.dispose();
-					JOptionPane.showMessageDialog(null, "Program Chrased :-( (Change to linux so that its dosn't crash)", "Alert", JOptionPane.ERROR_MESSAGE);
-					JOptionPane.showMessageDialog(null, "Thanks To all the people who dodn't help me make this, because you made me want to do it", "Thanks", JOptionPane.INFORMATION_MESSAGE);
-				}
-					//JOptionPane.showMessageDialog(null, message, "Message", JOptionPane.INFORMATION_MESSAGE);	
+		try {
+			if (commands[0].equalsIgnoreCase("close")) {
+				data.setText("Hacker");
+				JOptionPane.showMessageDialog(null, "Accses Granted", "Alert",
+						JOptionPane.INFORMATION_MESSAGE);
+				JOptionPane.showMessageDialog(null, "Nope", "Alert",
+						JOptionPane.INFORMATION_MESSAGE);
+				JOptionPane
+						.showMessageDialog(
+								null,
+								"Its just a binary converter - geez - Stop being a big baby",
+								"Alert", JOptionPane.INFORMATION_MESSAGE);
+				// JOptionPane.showMessageDialog(null,
+				// "Installing Virus...101010101", "Alert",
+				// JOptionPane.INFORMATION_MESSAGE);
+
+				frame.dispose();
+				JOptionPane
+						.showMessageDialog(
+								null,
+								"Program Chrased :-( (Change to linux so that its dosn't crash)",
+								"Alert", JOptionPane.ERROR_MESSAGE);
+				JOptionPane
+						.showMessageDialog(
+								null,
+								"Thanks To all the people who dodn't help me make this, because you made me want to do it",
+								"Thanks", JOptionPane.INFORMATION_MESSAGE);
 			}
-		 
-			catch (Exception ex)
-			{
-				
-			}
-		 }}
+			// JOptionPane.showMessageDialog(null, message, "Message",
+			// JOptionPane.INFORMATION_MESSAGE);
+		}
+
+		catch (Exception ex) {
+
+		}
+	}
+}
--- src/vafspy/weparr/Console.java
+++ src/vafspy/weparr/Console.java
@@ -1,6 +1,5 @@
 package vafspy.weparr;
 
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
@@ -28,286 +27,262 @@ import javax.swing.text.StyledDocument;
 
 /**
  * Test comment
+ * 
  * @author viktor
  *
  */
-public class Console
-{
-	public static void main(String[] args)
-	{
+public class Console {
+	public static void main(String[] args) {
 		new Console();
 	}
+
 	public JFrame frame;
 	public JTextPane console;
 	public JTextField input;
 	public JScrollPane scrollpane;
-	
+
 	public StyledDocument document;
-	
+
 	boolean trace = false;
-	
-	public Console()
-	{
-		try
-		{
+
+	public Console() {
+		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception ex) {
 		}
-		catch (Exception ex) {}
-		
+
 		frame = new JFrame();
 		frame.setTitle("Console");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		console = new JTextPane();
 		console.setEditable(false);
 		console.setFont(new Font("Courier New", Font.BOLD, 12));
 		console.setOpaque(false);
-		
+
 		document = console.getStyledDocument();
-		
-		
-		
+
 		input = new JTextField();
 		input.setEditable(true);
-		//input.setBorder(null);
-		//input.getContentPane().setBackground(new Color(50,50,50));
-		input.setBackground(new Color(50,50,50));
+		// input.setBorder(null);
+		// input.getContentPane().setBackground(new Color(50,50,50));
+		input.setBackground(new Color(50, 50, 50));
 		input.setForeground(Color.BLACK);
 		input.setCaretColor(Color.GREEN);
 		input.setOpaque(false);
 		input.setFont(new Font("Courier New", Font.BOLD, 12));
-		//input.getText();
-		
-		input.addActionListener(new ActionListener()
-		{
+		// input.getText();
+
+		input.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				
+			public void actionPerformed(ActionEvent e) {
+
 				String text = input.getText();
-				
-				
-				if (text.length() > 1)
-				{
-					print(text + "\n", false, new Color(255,0,0));
-				//	println(text, false);
+
+				if (text.length() > 1) {
+					print(text + "\n", false, new Color(255, 0, 0));
+					// println(text, false);
 					doCommand(text);
-			//	scrollBottom;
+					// scrollBottom;
 					input.selectAll();
-				}			
+				}
 			}
 		});
-		input.addKeyListener(new KeyListener()
-		{
+		input.addKeyListener(new KeyListener() {
 			@Override
-			public void keyTyped(KeyEvent e) {}
+			public void keyTyped(KeyEvent e) {
+			}
 
 			@Override
-			public void keyPressed(KeyEvent e) {}
+			public void keyPressed(KeyEvent e) {
+			}
 
 			@Override
-			public void keyReleased(KeyEvent e) {}
-			
-		});	
+			public void keyReleased(KeyEvent e) {
+			}
+
+		});
 		scrollpane = new JScrollPane(console);
 		scrollpane.setBorder(null);
 		scrollpane.setOpaque(false);
 		scrollpane.getViewport().setOpaque(false);
-		
+
 		frame.add(input, BorderLayout.SOUTH);
 		frame.add(scrollpane, BorderLayout.CENTER);
-		frame.getContentPane().setBackground(new Color(50,50,50));
-		
+		frame.getContentPane().setBackground(new Color(50, 50, 50));
+
 		frame.setSize(660, 350);
 		frame.setLocationRelativeTo(null);
-		
+
 		frame.setResizable(false);
 		frame.setVisible(true);
-		
-		//Starting Message
-		
-		 println("Starting Console ", trace, new Color(255,0,0));
-		 println("Console Started \n ", trace, new Color(0,200,0));
-		 println("Enter command to start: ", trace, new Color(0,200,0));
-		 try{
-			 
-			 String username = System.getProperty("user.name");
-		 String ip = InetAddress.getLocalHost().getHostAddress();
-			
-			//println("",ip, new Color(255,155,155));
+
+		// Starting Message
+
+		println("Starting Console ", trace, new Color(255, 0, 0));
+		println("Console Started \n ", trace, new Color(0, 200, 0));
+		println("Enter command to start: ", trace, new Color(0, 200, 0));
+		try {
+
+			String username = System.getProperty("user.name");
+			String ip = InetAddress.getLocalHost().getHostAddress();
+
+			// println("",ip, new Color(255,155,155));
 			System.out.println(username + "'s Ip: " + ip);
-		//	PrintWriter iplog = new PrintWriter("IpLog.txt");
-		//	iplog.print(text);
+			// PrintWriter iplog = new PrintWriter("IpLog.txt");
+			// iplog.print(text);
+		} catch (Exception ex) {
 		}
-		 catch (Exception ex) {}
 	}
-	
-	
-	public void scrollTop()
-	{
+
+	public void scrollTop() {
 		console.setCaretPosition(0);
 	}
-	
-	public void scrollBottom()
-	{
+
+	public void scrollBottom() {
 		console.setCaretPosition(console.getDocument().getLength());
 	}
-	
-	
-	public void print(String s, boolean trace)
-	{
-		print(s, trace, new Color(255,255,255));
+
+	public void print(String s, boolean trace) {
+		print(s, trace, new Color(255, 255, 255));
 	}
-	
-	public void print(String s, boolean trace, Color c)
-	{
+
+	public void print(String s, boolean trace, Color c) {
 		Style style = console.addStyle("Style", null);
 		StyleConstants.setForeground(style, c);
-		
-		if (trace)
-		{
+
+		if (trace) {
 			Throwable t = new Throwable();
 			StackTraceElement[] elements = t.getStackTrace();
 			String caller = elements[0].getClassName();
-			
+
 			s = caller + "-->" + s;
 		}
-		
-		try
-		{
+
+		try {
 			document.insertString(document.getLength(), s, style);
+		} catch (Exception ex) {
 		}
-		catch (Exception ex) {}
 	}
-	
-	public void println(String s, boolean trace)
-	{
-		println(s, trace, new Color(255,255,255));
+
+	public void println(String s, boolean trace) {
+		println(s, trace, new Color(255, 255, 255));
 	}
-	public void println(String s, boolean trace, Color c)
-	{
+
+	public void println(String s, boolean trace, Color c) {
 		print(s + "\n", trace, c);
 	}
-	
-	public void clear()
-	{
-		try
-		{
-			document.remove(0, document.getLength());
-		}
-		catch (Exception ex) {}
-		
-	}
-	
-	public void help()
-	{
-		 println("Help: ", trace, new Color(0,200,0));
-	}
-	public void getip()
-	{
-	try {                                                       
-		   //reference: http://stackoverflow.com/questions/2939218/getting-the-external-ip-address-in-java                                                                          
-		  URL whatismyip = new URL("http://htmlguy.cu.cc/ip_finder_for_weaponsarray.php");
-		  BufferedReader in = new BufferedReader(new InputStreamReader(
-		                  whatismyip.openStream())); 
-		  String ip = in.readLine();
-		  //reference: http://stackoverflow.com/questions/8765578/get-local-ip-address-without-connecting-to-the-internet
-		  System.out.println("second");
-		  Socket s = new Socket("google.ca", 80);
-		  String ip2 = s.getLocalAddress().getHostAddress();
-		  s.close();
-String ip3 = InetAddress.getLoopbackAddress().getHostAddress();
-String ip4 = InetAddress.getLoopbackAddress().getCanonicalHostName();
 
-String username = System.getProperty("user.name");
-System.out.println(username);
-		//println("",ip, new Color(255,155,155));
-		System.out.println("find ip: " + ip);
-		println("\n Found your IP addresses: ", trace, new Color(0,200,0));
-		println("Remote IP: " + ip, trace, new Color(0,200,0));
-		println("local LAN IP: " + ip2, trace, new Color(0,200,0));
-		println("localhost IP: " + ip3, trace, new Color(0,200,0));
-		println("localhost name: " + ip4, trace, new Color(0,200,0));
-		
-		
-		// Print IPs found to file!
-         try (PrintStream outiplog = new PrintStream(new FileOutputStream("/home/" + username + "/Desktop/iplog.txt"))) {
-        	 outiplog.println("\n Found your IP addresses: ");
-     		 outiplog.println("Remote IP: " + ip);
-     		 outiplog.println("local LAN IP: " + ip2);
-     		 outiplog.println("localhost IP: " + ip3);
-     		 outiplog.println("localhost name: " + ip4);
-     		 outiplog.close();
-        	}
+	public void clear() {
+		try {
+			document.remove(0, document.getLength());
+		} catch (Exception ex) {
+		}
+
 	}
-	catch (Exception ex){
-		println("Error -->" + ex.getMessage(), trace, new Color(255,155,155));
-		
+
+	public void help() {
+		println("Help: ", trace, new Color(0, 200, 0));
 	}
+
+	public void getip() {
+		try {
+			// reference:
+			// http://stackoverflow.com/questions/2939218/getting-the-external-ip-address-in-java
+			URL whatismyip = new URL(
+					"http://htmlguy.cu.cc/ip_finder_for_weaponsarray.php");
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					whatismyip.openStream()));
+			String ip = in.readLine();
+			// reference:
+			// http://stackoverflow.com/questions/8765578/get-local-ip-address-without-connecting-to-the-internet
+			System.out.println("second");
+			Socket s = new Socket("google.ca", 80);
+			String ip2 = s.getLocalAddress().getHostAddress();
+			s.close();
+			String ip3 = InetAddress.getLoopbackAddress().getHostAddress();
+			String ip4 = InetAddress.getLoopbackAddress()
+					.getCanonicalHostName();
+
+			String username = System.getProperty("user.name");
+			System.out.println(username);
+			// println("",ip, new Color(255,155,155));
+			System.out.println("find ip: " + ip);
+			println("\n Found your IP addresses: ", trace, new Color(0, 200, 0));
+			println("Remote IP: " + ip, trace, new Color(0, 200, 0));
+			println("local LAN IP: " + ip2, trace, new Color(0, 200, 0));
+			println("localhost IP: " + ip3, trace, new Color(0, 200, 0));
+			println("localhost name: " + ip4, trace, new Color(0, 200, 0));
+
+			// Print IPs found to file!
+			try (PrintStream outiplog = new PrintStream(new FileOutputStream(
+					"/home/" + username + "/Desktop/iplog.txt"))) {
+				outiplog.println("\n Found your IP addresses: ");
+				outiplog.println("Remote IP: " + ip);
+				outiplog.println("local LAN IP: " + ip2);
+				outiplog.println("localhost IP: " + ip3);
+				outiplog.println("localhost name: " + ip4);
+				outiplog.close();
+			}
+		} catch (Exception ex) {
+			println("Error -->" + ex.getMessage(), trace, new Color(255, 155,
+					155));
+
+		}
 	}
-	
-	public void hack(){
-		println("Started Hacking interface",trace,new Color(255,255,255));
+
+	public void hack() {
+		println("Started Hacking interface", trace, new Color(255, 255, 255));
 	}
-	
-	public void doCommand(String s)
-	{
-		
-	 final String[] commands = s.split(" ");
-	
-	 try
-		{
-			if (commands[0].equalsIgnoreCase("clear"))
-			{
+
+	public void doCommand(String s) {
+
+		final String[] commands = s.split(" ");
+
+		try {
+			if (commands[0].equalsIgnoreCase("clear")) {
 				clear();
 			}
-			
-			if (commands[0].equalsIgnoreCase("cls"))
-			{
+
+			if (commands[0].equalsIgnoreCase("cls")) {
 				clear();
 			}
-			if (commands[0].equalsIgnoreCase("getip"))
-			{
+			if (commands[0].equalsIgnoreCase("getip")) {
 				getip();
 			}
-			if (commands[0].equalsIgnoreCase("help"))
-			{
+			if (commands[0].equalsIgnoreCase("help")) {
 				help();
 			}
-			if (commands[0].equalsIgnoreCase("hack"))
-			{hack();}
-			if (commands[0].equalsIgnoreCase("what_is_your_opinion_on_me?"))
-			{
-				clear();
-				frame.setSize(700,700);
-				println("You are a big MEANIE, and she is very Selfish", trace, new Color(255,0,0));
-				input.setVisible(true);
+			if (commands[0].equalsIgnoreCase("hack")) {
+				hack();
 			}
-			else if (commands[0].equalsIgnoreCase("popup"))
-			{
+			if (commands[0].equalsIgnoreCase("what_is_your_opinion_on_me?")) {
+				clear();
+				frame.setSize(700, 700);
+				println("You are a big MEANIE, and she is very Selfish", trace,
+						new Color(255, 0, 0));
+				input.setVisible(true);
+			} else if (commands[0].equalsIgnoreCase("popup")) {
 				String message = "";
-				
-				for (int i = 1; i < commands.length; i++)
-				{
+
+				for (int i = 1; i < commands.length; i++) {
 					message += commands[1];
-					
-					if (i != commands.length -1)
-					{
+
+					if (i != commands.length - 1) {
 						message += " ";
 					}
 				}
-				JOptionPane.showMessageDialog(null, message, "Message", JOptionPane.INFORMATION_MESSAGE);
-				println(" \n \nPopup Generated:",trace);
+				JOptionPane.showMessageDialog(null, message, "Message",
+						JOptionPane.INFORMATION_MESSAGE);
+				println(" \n \nPopup Generated:", trace);
 			}
 		}
-	 
-		catch (Exception ex)
-		{
-			println("Error -->" + ex.getMessage(), trace, new Color(255,155,155));
+
+		catch (Exception ex) {
+			println("Error -->" + ex.getMessage(), trace, new Color(255, 155,
+					155));
 			println("Do 'help' for commands", trace);
 		}
 	}
-	
-	
-	
+
 }
